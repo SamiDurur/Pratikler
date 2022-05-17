1b5bc802c1428ff8cd827068b03960b232ff5445
Sami Durur <durursami@gmail.com>
2022-05-17 15:01:52 +0300

    Diziler

---
+++ diziler/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dizi Tanımlama
+            string[]renkler = new string[5];
+            
+            string []hayvanlar={"kedi","köpek","kuş","maymun"};
+
+            int[]dizi;
+            dizi= new int[5];
+
+            //Dizilere Değer Atama ve Erişim
+            renkler[0]="Mavi";
+            
+            dizi[3]=10;
+
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            //Döngülerle dizi kullanımı
+            
+            //Klavyeden girilen n tane sayının ortalamsını hesaplayan program
+            Console.Write("Dizinin Eleman sayısını giriniz: ");
+            int dizisayisi=int.Parse(Console.ReadLine());
+            int[] dizi1 =new int[dizisayisi];
+            
+            for (int i = 0; i < dizisayisi; i++)
+            {
+                Console.Write("{0}. Sayıyı gir : ",i+1);
+                dizi1[i]=int.Parse(Console.ReadLine());
+            }
+            int topla=0;
+            foreach (var item in dizi1)
+                topla+=item;
+            Console.WriteLine("Sayıların toplamı: "+topla);
+            Console.WriteLine("Sayıların Ortalaması: " +topla/dizisayisi);
+        }
+    }
+}
