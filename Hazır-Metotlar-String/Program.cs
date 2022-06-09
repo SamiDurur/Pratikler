d52be845150f18d84daaf6ea5ee3168d44e37d7e
Sami Durur <durursami@gmail.com>
2022-06-09 13:30:54 +0300

    Hazır Metotlar String

--- Hazır-Metotlar-String/Program.cs
+++ Hazır-Metotlar-String/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string degisken ="Dersimiz CSharp,Hoşgeldin";
-string degisken2="dersimiz CSharp,Hoşgeldin";
+string degisken ="Dersimiz CSharp, Hoşgeldin";
+string degisken2="dersimiz CSharp, Hoşgeldin";
 
 //Length; dizide veya stringde uzunluğunu verir
 Console.WriteLine(degisken.Length);
@@ -34,7 +34,31 @@ Console.WriteLine(degisken.LastIndexOf("i"));
 //insert Değişken içine ekleme hangi satırdan başlayıp sonrasında ne eklemesi gerekiyorsa onu yazarak ekler
 Console.WriteLine(degisken.Insert(0,"Merhaba "));
 
+//PadLeft, PadRight karakter uzunluğunu sağ veya sola boşluk atarak yazılan rakama tamamlar (20 tana karakter varsa 30 yazdığın zaman 10 tane boşluk atar sağına veya soluna)
+Console.WriteLine(degisken+degisken2.PadLeft(30));
+//Boşluk yerine başka karakterde seçilebilir
+Console.WriteLine(degisken.PadRight(40,'*')+degisken2);
+Console.WriteLine(degisken+degisken2.PadLeft(30,'3'));
+Console.WriteLine(degisken.PadRight(40,'_'));
 
+//Remove yazdığın indexten başlayarak  geri kalanını siler ve yazılan indexten öncesini ekrana getirir
+Console.WriteLine(degisken.Remove(20));
+//aradan silme yapmak içinde başlama index ini ve kaç karakter silinecek 
+Console.WriteLine(degisken.Remove(18,5));
+//baştakini silme
+Console.WriteLine(degisken.Remove(0,1));
+
+//Replace değiştirme işlemi yapar  ilk değişecek değeri 2.de olmasını istediğin değer
+Console.WriteLine(degisken.Replace("CSharp","C#"));
+Console.WriteLine(degisken.Replace(" ","*"));
+
+//Split Parçalayıp diziye atar.Boşluğuna göre  diziye atayıp 1. diziyi ekrana yazdırma
+Console.WriteLine(degisken.Split(" ")[1]);
+                                                                                                             //"Dersimiz CSharp, Hoşgeldin"
+//Substring Verilen index ten başlayarak sonuna kadar getiriyor (aşağıda 4. indexten başlayıp sonuna kadar getirdi "imiz CSharp, Hoşgeldin")
+Console.WriteLine(degisken.Substring(4));
+//Aradaki veriyi gösterme - 4.indexten başla 5 karakter getir
+Console.WriteLine(degisken.Substring(4,5));
 
 
 
