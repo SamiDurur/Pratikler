e448b1e22bd21dd374c7e2af140fb052a036446e
Sami Durur <durursami@gmail.com>
2022-07-29 21:15:53 +0300

    Kolesiyonlar-Soru-1 ve Dictionary

---
+++ Koleksiyonlar-Soru-1/Program.cs
@@ -0,0 +1,50 @@
+﻿//Soru - 1: Klavyeden girilen 20 adet pozitif sayının asal ve asal olmayan olarak 2 ayrı listeye atın. (ArrayList sınıfını kullanarak yazınız.)
+
+//Negatif ve numeric olmayan girişleri engelleyin.
+//Her bir dizinin elemanlarını büyükten küçüğe olacak şekilde ekrana yazdırın.
+//Her iki dizinin eleman sayısını ve ortalamasını ekrana yazdırın.
+
+using System.Collections;
+
+ArrayList asal = new ArrayList();
+ArrayList asalOlmayan = new ArrayList();
+for (int i = 0; i < 20; i++)
+{
+    uint sayi;
+    if (uint.TryParse(Console.ReadLine(), out sayi))
+    {
+        int control = 0;
+        for (int j = 2; j < sayi; j++)
+            if (sayi % j == 0)
+                control++;
+        if (control != 0)
+            asalOlmayan.Add(sayi);
+        else
+            asal.Add(sayi);
+    }
+    else
+        Console.WriteLine("Negatif veya Numeric olmayan giriş yaptınız");
+}
+asal.Sort();
+asalOlmayan.Sort();
+asal.Reverse();
+asalOlmayan.Reverse();
+Console.WriteLine("Asal Sayılar");
+foreach (var item in asal)
+    Console.WriteLine(item);
+Console.WriteLine("Asal Olmayan Sayılar");
+foreach (var item in asalOlmayan)
+    Console.WriteLine(item);
+int asalOrt = 0;
+int asalOlmayanOrt = 0;
+for (int i = 0; i < asal.Count; i++)
+{
+    asalOrt += Convert.ToInt32(asal[i]);
+}
+for (int i = 0; i < asalOlmayan.Count; i++)
+{
+    asalOlmayanOrt += Convert.ToInt32(asalOlmayan[i]);
+}
+
+Console.WriteLine(" Asal sayı adeti = " + asal.Count + " Asal sayıların ortalaması = " + asalOrt / asal.Count);
+Console.WriteLine(" Asal olmayan sayı adeti = " + asalOlmayan.Count + " Asal olmayan sayıların ortalaması = " + asalOlmayanOrt / asalOlmayan.Count);
---
+++ dictionary/Program.cs
@@ -0,0 +1,55 @@
+﻿//system.collection.generic
+//Liste türüdür.
+//her elemanı  key (anahtar) ve value (değer) olarak kaydeder
+
+//Key ler her elemanda uniq(tek) olması gerekiyor
+
+Dictionary<int, string> kullanıcılar = new Dictionary<int, string>();
+kullanıcılar.Add(11, "Sami DURUR");
+kullanıcılar.Add(12, "Umit");
+kullanıcılar.Add(13, "Ramazan");
+kullanıcılar.Add(33, "Omer");
+kullanıcılar.Add(34, "Burcu");
+
+//dizinin elemanlarına erişim
+
+Console.WriteLine("**-** Elemanlara Erişim **-**");
+Console.WriteLine(kullanıcılar[12]);
+
+foreach (var item in kullanıcılar)
+    Console.WriteLine(item);
+
+//Count - Kaç elemandan oluşur
+Console.WriteLine("**-** Count **-**");
+
+Console.WriteLine(kullanıcılar.Count);
+
+//Contains - Bu elemandan  listede varmı
+Console.WriteLine("**-** Contains **-**");
+
+Console.WriteLine(kullanıcılar.ContainsKey(11));
+Console.WriteLine(kullanıcılar.ContainsValue("Vural"));
+
+//Remove - Silme
+Console.WriteLine("**-** Remove **-**");
+
+kullanıcılar.Remove(12);
+
+foreach (var item in kullanıcılar)
+{
+    Console.WriteLine(item);
+    Console.WriteLine(item.Key);
+    Console.WriteLine(item.Value);
+}
+
+
+//Keys
+Console.WriteLine("**-** Keys **-**");
+foreach (var item in kullanıcılar.Keys)
+    Console.WriteLine(item);
+
+
+//values
+Console.WriteLine("**-** Values **-**");
+foreach (var item in kullanıcılar.Values)
+    Console.WriteLine(item);
