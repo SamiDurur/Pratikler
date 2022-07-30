7a367ac0d52e8bba14442654efa9f86ef1294846
Sami Durur <durursami@gmail.com>
2022-07-30 12:21:57 +0300

    Koleksiyon-Soru-3

---
+++ Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+string cumle=Console.ReadLine();
+string sesli = "aeoöuüıi";
+List<char> sesliler = new List<char>();
+
+for(int i=0;i<cumle.Length;i++)
+{
+    if (sesli.Contains(cumle[i])) 
+    {
+        sesliler.Add(cumle[i]); 
+    }
+    
+}
+Console.WriteLine("Sesli harfler");
+foreach (var item in sesliler)
+    Console.Write(item);
+
+sesliler.Sort();
+Console.WriteLine("Sıralı Sesli harfler");
+foreach (var item in sesliler)
+    Console.Write(item);
